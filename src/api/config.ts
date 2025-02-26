353e094e004affab833fe08c30ac1b6f913cf195
KevinGomes2004 <kevingomesmathra@gmail.com>
2025-02-27 04:45:19 +0530

    API configuration

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,5 +1,5 @@
 // Base API configuration
 export const API_CONFIG = {
-    BASE_URL: 'https://your-backend-url.com/api',
-    // Add other API configurations
-  };
+  BASE_URL: 'https://your_backend-url.com/api',
+  // Add other API configurations
+};
