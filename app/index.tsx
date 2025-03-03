adf245d24d858022140073b68dfee48084ffb528
dinukan93 <dinukan02@gmail.com>
2025-03-03 09:49:18 +0530

    Added project setup
    Uploaded all initial project files

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { Link } from 'expo-router';
+
+const Index = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+    </View>
+  );
+}
+
+export default Index;
+
