8890898aa67b0caefc3e6f5ba42fbe804cfd2913
Jithnuka Athurugiriya <jithnukanimandith2002@gmail.com>
2025-03-04 08:54:12 +0530

    Group Tracking connected

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -6,18 +6,20 @@ import Loging from '../screens/Loging';
 import ForgotPassword from '../screens/ForgotPassword';
 import Load from '../screens/Load';
 import Profile from '../screens/Profile';
+import GroupTrackingScreen from '../screens/GroupTrackingScreen';
 
 const Stack = createStackNavigator();
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Profile" id={undefined}>
+      <Stack.Navigator initialRouteName="GroupTrackingScreen" id={undefined}>
         <Stack.Screen name="Load" component={Load} options={{ headerShown: false }} />
         <Stack.Screen name="Loging" component={Loging} options={{ headerShown: false }} />
         <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }} />
         <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{ headerShown: false }} />
         <Stack.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
+        <Stack.Screen name="GroupTrackingScreen" component={GroupTrackingScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
