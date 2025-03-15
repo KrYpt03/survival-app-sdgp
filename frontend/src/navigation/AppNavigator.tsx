5bfc41df0b15279b63a15cb03d7c0bf3ff181609
akindu415 <akindu415@gmail.com>
2025-03-16 01:40:33 +0530

    Implement Signup page using clerk

---
+++ frontend/src/navigation/AppNavigator.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SignedIn, SignedOut } from '@clerk/clerk-expo';
+import SignUp from '../screens/SignUp';
+import Loging from '../screens/Loging';
+import ForgotPassword from '../screens/ForgotPassword';
+import Load from '../screens/Load';
+import Profile from '../screens/Profile';
+import GroupTrackingScreen from '../screens/GroupTrackingScreen';
+import Home from '../screens/Home';
+
+const Stack = createStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+// If signed out users see Load Loging SignUp ForgotPassword
+      <SignedOut>
+        <Stack.Navigator initialRouteName='SignUp' id={undefined}>
+        <Stack.Screen name="Load" component={Load} options={{ headerShown: false }} />
+        <Stack.Screen name="Loging" component={Loging} options={{ headerShown: false }} />
+        <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </SignedOut>
+
+// If signed in users see Home Profile GroupTrackingScreen
+      <SignedIn>
+        <Stack.Navigator initialRouteName="Home" id={undefined}>
+          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+          <Stack.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
+          <Stack.Screen name="GroupTrackingScreen" component={GroupTrackingScreen} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </SignedIn>
+    </NavigationContainer>
+  );
+}
+
