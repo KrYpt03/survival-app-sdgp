6e121f0a06f1a1811465e57a98e8b537a6054332
akindu415 <akindu415@gmail.com>
2025-03-18 15:36:59 +0530

    deleting middleware to merge with the backend

--- backend/src/api/middlewares/auth.ts
+++
@@ -1,9 +0,0 @@
-import { ClerkExpressWithAuth } from "@clerk/clerk-sdk-node";
-import { NextFunction, Request, Response } from "express";
-
-export const authenticate = ClerkExpressWithAuth({
-  onError: (err: any) => {
-    return { status: 401, body: { error: "Unauthorized", details: err.message } };
-  },
-});
-
