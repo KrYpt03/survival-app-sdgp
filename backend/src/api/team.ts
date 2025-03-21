e40fa5705750204075b38290d84f34c6c297167a
KrYpt03 <thisu2003@gmail.com>
2025-03-21 20:34:36 +0530

    feat: add authentication middleware to team API routes
    - Integrated authentication checks for all team-related endpoints to enhance security.
    - Updated GET, POST, PUT, and DELETE routes to require user authentication before processing requests.

--- backend/src/api/team.ts
+++ backend/src/api/team.ts
@@ -8,54 +8,54 @@ const router = express.Router();
  * Get all teams
  * GET /api/team
  */
-router.route("/").get( getAllTeams);
+router.route("/").get(isAuthenticated, getAllTeams);
 
 /**
  * Get team members with latest location
  * GET /api/team/:teamID/members
  */
-router.route("/:teamID/members").get( getTeamMembers);
+router.route("/:teamID/members").get( isAuthenticated, getTeamMembers);
 
 /**
  * Create a new team
  * POST /api/team
  */
-router.route("/").post(createTeam);
+router.route("/").post(isAuthenticated, createTeam);
 
 /**
  * Remove a team member
  * DELETE /api/team/member
  */
-router.route("/member").delete(removeTeamMember);
+router.route("/member").delete(isAuthenticated, removeTeamMember);
 
 /**
  * Change team leader
  * PUT /api/team/leader
  */
-router.route("/leader").put( changeTeamLeader);
+router.route("/leader").put(isAuthenticated, changeTeamLeader);
 
 /**
  *  Leave current team
  * POST /api/team/leave
  */
-router.route("/leave").post( leaveTeam);
+router.route("/leave").post(isAuthenticated, leaveTeam);
 
 /**
  * Deactivate team
  * PUT /api/team/deactivate
  */
-router.route("/deactivate").put( deactivateTeam);
+router.route("/deactivate").put(isAuthenticated, deactivateTeam);
 
 /**
  * Activate team
  * PUT /api/team/activate
  */
-router.route("/activate").put( activateTeam);
+router.route("/activate").put(isAuthenticated, activateTeam);
 
 /**
  * Join a team using team code
  * POST /api/team/join
  */
-router.route("/join").post( joinTeam);
+router.route("/join").post(isAuthenticated, joinTeam);
 
 export default router;
