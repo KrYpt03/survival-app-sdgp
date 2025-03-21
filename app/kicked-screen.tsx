d60165624b6c80e5ecb4ee0bcaf53174d9adbb8c
Jithnuka Athurugiriya <jithnukanimandith2002@gmail.com>
2025-03-21 12:07:41 +0530

    Kicked screen assets updated

--- app/kicked-screen.tsx
+++ app/kicked-screen.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import type React from "react"
+import React from "react"
 import { useEffect } from "react"
 import { View, Text, StyleSheet, ImageBackground, Dimensions, StatusBar } from "react-native"
 import { useNavigation } from "@react-navigation/native"
